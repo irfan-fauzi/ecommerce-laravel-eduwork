bedacf4eae6005f4255f0979bcedbec7f58f7c4a
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-10-14 16:16:37 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,21 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
 
 // Route::get('/user/{id}', function ($id) {
 //     return "user id:" . $id;
 // });
-
 
 // akan muncul too many request jika kita melakukan request ke halaman about 5 kali dalam 1 menit
 // Route::middleware('throttle:5,1')->get('/about', function () {
