b82e677f3941fe2eca923d12af343973ed23212a
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-10-14 15:53:27 +0700

    instal tailwind

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+// Route::get('/user/{id}', function ($id) {
+//     return "user id:" . $id;
+// });
+
+
+// akan muncul too many request jika kita melakukan request ke halaman about 5 kali dalam 1 menit
+// Route::middleware('throttle:5,1')->get('/about', function () {
+//     return view('about');
+// });
