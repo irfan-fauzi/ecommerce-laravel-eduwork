c9dd8a6d7fd332c45728a61e74a765cccd4342a3
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-10-16 20:36:01 +0700

    add all

--- database/migrations/2025_10_15_185426_create_products_table.php
+++ database/migrations/2025_10_15_185426_create_products_table.php
@@ -11,8 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->integer('stock');
+            $table->integer('price');
+           
             $table->timestamps();
         });
     }
@@ -22,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_product');
+        Schema::dropIfExists('products');
     }
 };
