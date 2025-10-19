6c4ff32085e12959347bf2abbc55fd9f24f5de53
irfan-fauzi <irfanwintech@yahoo.com>
2025-10-19 15:29:11 +0700

    img2

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>  
   <x-navbar></x-navbar>
-  <main class="h-[400px] bg-[url()] bg-no-repeat bg-cover">
+  <main class="h-[600px] bg-[url(https://raw.githubusercontent.com/irfan-fauzi/ecommerce-laravel-eduwork/refs/heads/main/public/img/cover.png)] bg-no-repeat bg-cover">
     <header>
 
     </header>
