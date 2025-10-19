8334ba2d3599a5c4ad8d5a7add51fe473369f361
irfan-fauzi <irfanwintech@yahoo.com>
2025-10-19 15:25:48 +0700

    add bg

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>  
   <x-navbar></x-navbar>
-  <main class="h-[200px] bg-card-blue">
+  <main class="h-[400px] bg-[url()] bg-no-repeat bg-cover">
     <header>
 
     </header>
