15ab7508f9b7cf5aa301bdf6f460041d6dd2bc2c
irfan-fauzi <irfanwintech@yahoo.com>
2025-11-11 03:11:11 +0700

    resize logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,19 @@
                 <li class="font-semibold hover:text-blue-600"><a href="/products">Products</a></li>
                 <li class="font-semibold hover:text-blue-600"><a href="/cart">Cart</a></li>
                 <li class="font-semibold hover:text-blue-600"><a href="#">Admin</a></li>
+                @if (Route::has('login'))
+                    @auth
+                        <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
+                    @else
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @endif
+                    @endauth
+                @endif
+
             </ul>
+
         </div>
     </div>
 </nav>
